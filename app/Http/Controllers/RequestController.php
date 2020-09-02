cddc98e5afa9dfb235044ca28cf5d76adbab35ac
Firzan97 <FirzanAzrai97@gmail.com>
2020-09-02 22:32:07 +0800

    fix naming Request Model in Request Controller into Req :bugs:

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Request as Req;
 
 class RequestController extends Controller
 {
@@ -25,7 +26,9 @@ class RequestController extends Controller
     public function create(Request $request)
     {
         //
-        $created = Request::create($request->all());
+
+
+        $created = Req::create($request->all());
     }
 
     /**
