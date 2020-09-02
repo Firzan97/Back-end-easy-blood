49284f267140d768d38dda2c3fa32840c427ff2d
Firzan97 <FirzanAzrai97@gmail.com>
2020-09-02 11:58:47 +0800

    added request & migrate :sparkles:

--- app/Request.php
+++ app/Request.php
@@ -9,4 +9,7 @@ class Request extends Model
     protected $collection = 'request';
     protected $primaryKey = 'id';
     protected $connection = 'mongodb';
+    protected $fillable = [
+        'location', 'bloodGroup', 'reason', 'user_id'
+    ];
 }
