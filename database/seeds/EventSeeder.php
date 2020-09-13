e2f390840dbeac057e5686f10d41357f160ebf9f
Firzan97 <FirzanAzrai97@gmail.com>
2020-09-14 00:29:22 +0800

    added seeder for event,user,donation,request, and rule

---
+++ database/seeds/EventSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        // DB::table('events')->insert([
+        //     'name' => Str::random(10),
+        //     'location' => Str::random(10).'@gmail.com',
+        //     'phoneNum' => Hash::make('password'),
+        // ]);
+    }
+}
