17e8f0f1a61920e2103ebe8d4f58354f534d14a0
Firzan97 <FirzanAzrai97@gmail.com>
2020-09-17 03:38:43 +0800

    fixed curl connection problem by adding         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -46,12 +46,16 @@ class NotificationController extends Controller
         ];
 
 
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $fcmURL);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_VERBOSE, true);
+
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
         $result = curl_exec($ch);
         curl_close($ch);
