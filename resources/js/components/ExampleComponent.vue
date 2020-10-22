f9dd535ae1356d547762aa4f4a518981594a7644
Firzan97 <FirzanAzrai97@gmail.com>
2020-10-23 03:32:40 +0800

    added event to the table

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="container">
+    <div class="container-fluid">
+        <div class="row">
+            <b-table striped hover :items="event"></b-table>
+        </div>
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
@@ -15,9 +18,32 @@
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+const axios = require("axios");
+export default {
+    data() {
+        return {
+            event: []
+        };
+    },
+    methods: {
+        loadUser() {
+            axios
+                .get("http://localhost:8000/api/event")
+                .then(response => {
+                    this.event = response.data;
+                    console.log(response.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         }
+    },
+    created() {
+        this.loadUser();
+        console.log("Component mounted.");
+    },
+    mounted() {
+        console.log("babsabsabjasbja");
     }
+};
 </script>
